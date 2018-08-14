fad77a21f001b2804a3ba5ec452fa86ebf6aacc0
Miguel Martinez <slverwolf@gmail.com>
2018-08-13 22:15:11 -0500

    Import build scripts

---
+++ build/anykernel.sh
@@ -0,0 +1,45 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=HolyDragon Kernel for Oreo Custom Roms for the OnePlus 5/T by HolyAngel, based on Sayved's fantastic work Dark Matter kernel.
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus5
+device.name2=OnePlus5T
+device.name3=cheeseburger
+device.name4=dumpling
+device.name5=
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## Trim partitions
+fstrim -v /cache
+fstrim -v /data
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+# begin ramdisk changes
+insert_line init.rc "init.darkmatter.rc" before "import /init.environ.rc" "import /init.darkmatter.rc";
+# end ramdisk changes
+write_boot;
+
+## end install
+
